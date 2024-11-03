81016669d1f29f647de4e89e7fcbdaa226a54f67
hussain jamil <hosen3919@gmail.com>
2024-11-03 12:58:19 +0200

    first commit

---
+++ js/iphone.js
@@ -0,0 +1,11 @@
+let imag = document.querySelector('.imag');
+let container = document.querySelector('.container');
+
+
+function changePhone(phone) {
+    imag.src = phone;
+}
+
+function changeBackGround(color) {
+    container.style.backgroundColor = color;
+}
